d372ff270db49a8ccbf2391e3630756376fb0505
Windmost8 <mostapha.elansary@gmail.com>
2021-06-01 14:55:25 +0000

    typo

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -118,7 +118,7 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
-    DATABASEs = {
+    DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
